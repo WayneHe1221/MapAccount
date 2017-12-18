04f41b830070c13d2d639acee3cfb1ad62db47bf
Unknown <rmp4m3jun@gmail.com>
2017-12-19 01:18:26 +0800

    new fragment

---
+++ app/src/main/java/com/chun/mapaccount/Day_coast_view.java
@@ -0,0 +1,8 @@
+package com.chun.mapaccount;
+
+/**
+ * Created by rmp4m on 2017/12/18.
+ */
+
+public class Day_coast_view {
+}
---
+++ app/src/main/java/com/chun/mapaccount/NoScrollViewPager.java
@@ -0,0 +1,8 @@
+package com.chun.mapaccount;
+
+/**
+ * Created by rmp4m on 2017/12/19.
+ */
+
+public class NoScrollViewPager {
+}
