9ddfab90f26c7d3f07415685720a41e8cc4d1395
Wayne He <thumbe31949@gmail.com>
2018-01-07 13:43:35 +0800

    Solve the conflict in SetPlace

--- app/src/main/java/com/chun/mapaccount/SetPlace.java
+++ app/src/main/java/com/chun/mapaccount/SetPlace.java
@@ -1,22 +1,41 @@
 package com.chun.mapaccount;
 
-import android.app.Activity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * Created by Wayne on 2017/12/20.
  */
 
-public class SetPlace extends Activity {
+public class SetPlace extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
     private Button finish_setplace;
+
+    private GoogleMap mMap;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_place);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         finish_setplace = (Button) findViewById(R.id.finish_setplace);
         finish_setplace.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +44,31 @@ public class SetPlace extends Activity {
                 SetPlace.this.finish();
             }
         });
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE, Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+        LatLng utaipei = new LatLng(25.035810, 121.513746);
+        mMap.addMarker(new MarkerOptions().position(utaipei).title("天大地大北市大"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(utaipei, 17));
     }
 }
